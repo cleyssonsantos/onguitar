8d730b2885571f455b2ee38a4e6894dcb73a5925
Cleysson Santos <69848728+cleyssonsantos@users.noreply.github.com>
2024-07-18 22:33:02 +0000

    Adjusting connection db in try catch

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -14,7 +14,7 @@ async function status(request, response) {
     values: [databaseName]
   })
   const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
-  
+
   response.status(200).json({
     updated_at: updatedAt,
     dependencies: {
