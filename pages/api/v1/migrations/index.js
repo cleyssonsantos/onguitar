1e54ed1e59f18c12f1ee63f57bc4414b254f827d
Cleysson Santos <69848728+cleyssonsantos@users.noreply.github.com>
2024-07-24 18:09:00 +0000

    fix `/migrations` endpoint connection bug

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -3,6 +3,13 @@ import { join } from "node:path";
 import database from "infra/database.js";
 
 export default async function migrations(request, response) {
+  const allowedMethods = ["GET", "POST"];
+  if (!allowedMethods.includes(request.method)) {
+    return response.status(405).json({
+      error: `Method "${request.method}" not allowed`,
+    });
+  }
+
   let dbClient;
 
   try {
@@ -19,7 +26,6 @@ export default async function migrations(request, response) {
 
     if (request.method === "GET") {
       const pendingMigrations = await migrationRunner(defaultMigrationOption);
-      await dbClient.end();
       return response.status(200).json(pendingMigrations);
     }
 
@@ -29,15 +35,11 @@ export default async function migrations(request, response) {
         dryRun: false,
       });
 
-      await dbClient.end();
-
       if (migratedMigrations.length > 0) {
         return response.status(201).json(migratedMigrations);
       }
       return response.status(200).json(migratedMigrations);
     }
-
-    return response.status(405).end();
   } catch (error) {
     console.error(error);
     throw error;
