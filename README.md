e1613b3c01b90dca5c2bbdea44524426c29d8301
Cleysson Santos <69848728+cleyssonsantos@users.noreply.github.com>
2024-07-29 12:08:50 +0000

    fix `prettier` style linting

--- README.md
+++ README.md
@@ -5,17 +5,22 @@ Projeto que visa criar um espaço de valor para os amantes da guitarra.
 # Como executar
 
 ## Instalando dependências
+
 nvm install - Buscará o arquivo .nvmrc "run commands"
 
 ## Para instalar as dependências do package.json
+
 npm install
 
 ## Rodando o projeto local
+
 npm run dev
 
 ## Executando os testes
+
 npm run test
 npm run test:watch
 
 ## Subindo banco de dados
+
 docker compose -f infra/compose.yaml up -d
--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -3,17 +3,21 @@ import database from "infra/database.js";
 async function status(request, response) {
   const updatedAt = new Date().toISOString(); // No formato ISO8601
   const databaseVersionResult = await database.query("SHOW server_version;");
-  const databaseVersionValue = databaseVersionResult.rows[0].server_version
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
 
-  const databaseMaxConnectionsResult = await database.query("SHOW max_connections;")
-  const databaseMaxConnectionsValue = databaseMaxConnectionsResult.rows[0].max_connections
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
 
-  const databaseName = process.env.POSTGRES_DB
+  const databaseName = process.env.POSTGRES_DB;
   const databaseOpenedConnectionsResult = await database.query({
     text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1;",
-    values: [databaseName]
-  })
-  const databaseOpenedConnectionsValue = databaseOpenedConnectionsResult.rows[0].count
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResult.rows[0].count;
 
   response.status(200).json({
     updated_at: updatedAt,
@@ -22,8 +26,8 @@ async function status(request, response) {
         version: databaseVersionValue,
         max_connections: parseInt(databaseMaxConnectionsValue),
         open_connections: databaseOpenedConnectionsValue,
-      }
-    }
+      },
+    },
   });
 }
 
